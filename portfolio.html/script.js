09e1d3a0a1314a5b22452158745a1d7c65ad81fc
bernardgyedu <73842278+bernardgyedu@users.noreply.github.com>
2021-08-07 13:10:22 -0400

    major changes
    added more functionality to the page

--- portfolio.html/script.js
+++ portfolio.html/script.js
@@ -3,3 +3,49 @@ let spinnerWrapper = document.querySelector('.loading-screen')
 window.addEventListener('load' , function (){
     spinnerWrapper.parentElement.removeChild(spinnerWrapper)
 })
+
+/* start intro text */
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml13');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml13 .letter',
+    translateY: [100,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 300 + 30 * i
+  }).add({
+    targets: '.ml13 .letter',
+    translateY: [0,-100],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1200,
+    delay: (el, i) => 100 + 30 * i
+  });
+/* intro effect 2 */
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml16');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml16 .letter',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 30 * i
+  }).add({
+    targets: '.ml16',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+/* end intro text */
